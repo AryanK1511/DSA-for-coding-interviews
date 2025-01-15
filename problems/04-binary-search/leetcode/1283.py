28e3f2b0ed76967227631ee6afa11343f9d6b63f
Aryan Khurana <aryankhurana1511@gmail.com>
2025-01-15 15:29:04 -0500

    Add a binary search question

--- problems/04-binary-search/leetcode/1283.py
+++ problems/04-binary-search/leetcode/1283.py
@@ -1,6 +1,26 @@
+from math import ceil
 from typing import List
 
 
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
-        pass
+        start, end = 1, max(nums)
+
+        def getSum(mid):
+            return sum(ceil(num / mid) for num in nums)
+
+        while start <= end:
+            mid = start + ((end - start) // 2)
+
+            if getSum(mid) > threshold:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return start
+
+
+sol = Solution()
+nums = [1, 2, 5, 9]
+threshold = 6
+print(sol.smallestDivisor(nums, threshold))
